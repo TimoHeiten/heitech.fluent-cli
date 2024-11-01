142a57f31c4817a4b66260efe53888bb7b1b037c
theitendotnet <80585677+theitendotnet@users.noreply.github.com>
2024-11-01 10:34:09 +0100

    Add publish-package and

---
+++ publish-package.ps1
@@ -0,0 +1,25 @@
+param(
+    [string] $apiKey
+)
+
+function checkExitCode([string] $message) {
+    $exitCode = $LastExitCode
+    if ($exitCode -ne 0) {
+        Write-Host "Error: $message"
+        exit $exitCode
+    }
+}
+
+# build sln
+dotnet build -c Release
+checkExitCode "Build failed"
+
+# test sln
+dotnet test
+checkExitCode "Test failed"
+
+# create package without build
+dotnet pack .\src\heitech-fluent-cli\heitech-fluent-cli.csproj -c Release
+
+# publish package
+dotnet nuget push .\src\bin\Release\heitech-fluent-cli.1.0.0.nupkg --api-key $apiKey --source https://api.nuget.org/v3/index.json
--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,24 @@
-# publish script
-dotnet publish -c Release
+
+param(
+    [string] $c = "Debug"
+)
+
+function checkExitCode([string] $message) {
+    $exitCode = $LastExitCode
+    if ($exitCode -ne 0) {
+        Write-Host "Error: $message"
+        exit $exitCode
+    }
+}
+
+# clean up
+dotnet clean
+checkExitCode "Clean failed"
+
+# build sln
+dotnet build -c $c
+checkExitCode "Build failed"
+
+# test sln
+dotnet test
+checkExitCode "Test failed"
